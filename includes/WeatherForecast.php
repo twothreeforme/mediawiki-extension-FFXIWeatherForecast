e41661b64a4e8cee2895ba69ce54248ef37a6e60
twothreeforme <72524280+twothreeforme@users.noreply.github.com>
2024-09-08 15:53:17 -0400

    Update WeatherForecast.php

--- includes/WeatherForecast.php
+++ includes/WeatherForecast.php
@@ -179,7 +179,7 @@ class SpecialWeatherForecast extends SpecialPage {
 		$html .= "<br>
 		<div ><i> All data and probabilities are based on AirSkyBoat. All earth times are based on your local timezone.</i></div>
 		<div style=\"max-height: 900px; overflow: auto; display: inline-block; width: 100%;\">
-		<table class=\"special-weatherforecast-table\">
+		<table class=\"special-weatherforecast-table sortable\">
 			<tr><th>Zone Name</th>
 			<th>Vana Days</th>
             <th>Day's Element</th>
