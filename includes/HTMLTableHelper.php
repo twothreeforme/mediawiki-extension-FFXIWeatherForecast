a9d2ecdb27ab57a53322d5d1180a49a488b9a415
twothreeforme <72524280+twothreeforme@users.noreply.github.com>
2024-09-14 19:56:24 -0400

    Update HTMLTableHelper.php

--- includes/HTMLTableHelper.php
+++ includes/HTMLTableHelper.php
@@ -9,7 +9,7 @@ class HTMLTableHelper {
 		 */
 		$html .= "<br>
 		<div ><i> All data and probabilities are based on AirSkyBoat. All earth times are based on your local timezone.</i></div>
-		<div style=\"max-height: 900px; overflow: auto; display: inline-block; width: 100%;\">
+		<div style=\"max-height: 400px; overflow: auto; display: inline-block; width: 100%; position: relative; overflow: scroll;\">
 		<table id=\"special-weatherforecast-table\" class=\"horizon-table general-table table-md special-weatherforecast-table  sortable\">
             <tr><th>Zone Name</th>
 			<th>Vana Days</th>
