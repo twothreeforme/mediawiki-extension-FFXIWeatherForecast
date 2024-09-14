92cd5d48ce0d172b64e8703f0e5b891316c95d7a
twothreeforme <72524280+twothreeforme@users.noreply.github.com>
2024-09-14 08:30:34 -0400

    Update DiggingWeatherForecast.php

--- includes/DiggingWeatherForecast.php
+++ includes/DiggingWeatherForecast.php
@@ -42,7 +42,7 @@ class SpecialDiggingWeatherForecast extends SpecialPage {
 
                 $vt = $time->getVanaTimeFromDaysAhead($key);
                 $vanadays = ( $key == 0 ) ? "0 (Today)" : $key;
-                print_r($time->dayColor($vt) . " ");
+                //print_r($time->dayColor($vt) . " ");
                 //$html .= "<tr><td>". $row['name'] ."</td><td style=\"text-align:center;\">$vanadays</td><td>" . $time->earthTime(null) . "</td><td style=\"text-align:center; color:" . $time->dayColor($vt) . "\" >" . $time->getWeekDayElement($key) .  "</td><td style=\"text-align:center;\">" . $time->moonPhaseNameFrom($key) . "</td><td>". $day['normal']. "</td><td>". $day['common']. "</td><td>". $day['rare']. "</td>";
                 $html .= "<tr><td>". $row['name'] ."</td><td style=\"text-align:center;\">$vanadays</td><td style=\"text-align:center; color:" . $time->dayColor($vt) . "\" >" . $time->getWeekDayElement($key) .  "</td><td style=\"text-align:center;\">" . $time->moonPhaseNameFrom($key) . "</td><td>". $day['normal']. "</td><td>". $day['common']. "</td><td>". $day['rare']. "</td>";
             }
